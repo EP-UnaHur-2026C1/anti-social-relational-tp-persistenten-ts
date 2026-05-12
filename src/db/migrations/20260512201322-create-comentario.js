c1093ba47068c22ecfca2fb33fae17f6e599d2c8
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-05-12 17:17:25 -0300

    Creada estructura base del proyecto, configuración de la db y creacion de modelos

---
+++ src/db/migrations/20260512201322-create-comentario.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comentarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idComentario: {
+        type: Sequelize.INTEGER
+      },
+      fechaPublicacion: {
+        type: Sequelize.DATE
+      },
+      esVisible: {
+        type: Sequelize.BOOLEAN
+      },
+      descripcion: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comentarios');
+  }
+};
